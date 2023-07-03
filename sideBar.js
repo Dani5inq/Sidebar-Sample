bc17862d13ac797316f9ee5d6639b6cd2f01179c
Dani5inq <yungwizzy18@gmail.com>
2023-07-03 15:10:25 +0100

    first commit

---
+++ sideBar.js
@@ -0,0 +1,10 @@
+const bars = document.querySelector(".fa-bars");
+const sidebar = document.querySelector(".sidebar");
+const times = document.querySelector(".fa-xmark");
+bars.addEventListener("click", () => {
+  sidebar.classList.toggle("show-sidebar");
+});
+
+times.addEventListener("click", () => {
+  sidebar.classList.remove("show-sidebar");
+});
